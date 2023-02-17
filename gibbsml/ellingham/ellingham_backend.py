128a59389b003be72df78df0c3a12927546acb85
mongchan2 <124707742+mongchan2@users.noreply.github.com>
2023-02-17 17:29:51 +0900

    Update ellingham_backend.py

--- gibbsml/ellingham/ellingham_backend.py
+++ gibbsml/ellingham/ellingham_backend.py
@@ -1,7 +1,7 @@
 import os
-from catlearn.regression.gaussian_process import GaussianProcess
+from catlearn.regression.gaussian_process import GaussianProcess # GPR 을 통해서 ML을 진행할 것이므로 
 
-from .fingerprint import Fingerprint
+from .fingerprint import Fingerprint # 같은 디렉토리 내에 존재하는 Fingerprint 파일을 참고 
 
 __author__ = "Jose A. Garrido Torres, Alexander Urban"
 __email__ = "aurban@atomistic.net"
@@ -24,16 +24,21 @@ class Ellingham():
           p_O2, p_CO, p_CO2: O2, CO, and CO2 partial pressures
 
         """
-
+        # 3성분계까지 가능한듯. 일단은 Oxigen bases로 진행되는것을 파악
+        
+        # API 키를 바탕으로, train_fp를 생성하고, 
         self.user_api_key = USER_API_KEY
         train_fp = Fingerprint(USER_API_KEY=self.user_api_key)
-
+      
+        # Training set를 불러온다 
         path_mod = os.path.dirname(__file__)
         train_fp.load_set(path_mod + '/trainingset_ellingham_08June2020.json')
-
+        
+        # Test set 생성 
         test_fp = Fingerprint(USER_API_KEY=self.user_api_key)
-
-        # Get training data.
+        
+        # training 데이터 추출. 주어진 input value와, 정답 label인 train_y를 매칭하고 생성하는 과정인듯 
+        # Get training data
         train_x = train_fp.get_features_values()
         train_y = train_fp.get_target_features_values(
             target_feature='dS0_expt')
@@ -43,7 +48,7 @@ class Ellingham():
             {'type': 'gaussian', 'width': 1., 'scaling': 1.},
             {'type': 'linear', 'scaling': 1., 'constant': 1.}]
 
-        # Train the GP model.
+        # Train the GP model. 이전에 만든 input value를 바탕으로 
         gp = GaussianProcess(kernel_list=kernel, regularization=1e-3,
                              regularization_bounds=(1e-5, 1e-1),
                              train_fp=train_x, train_target=train_y,
@@ -70,7 +75,8 @@ class Ellingham():
         self.balanced_reaction = test_fp.fp[label]['balanced reaction']
         self.dH0 = test_fp.get_feature_value('formation energy (kJ/mol)')[0][0]
         self.dS0 = pred
-
+    
+    # 여기부터는 주어진 데이터를 바탕으로 return 
     def get_dG0(self, T):
         dG0 = self.dH0 + T * self.dS0
         return dG0
